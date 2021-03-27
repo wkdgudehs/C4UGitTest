a629d54547223cca1de463fb7ace7352438eb508
LAPTOP-HU6VO98T\user <boram_kim57@hanmail.net>
2021-03-27 13:00:58 +0900

    JustTest수정

--- c4uGitTest/src/main/java/com/my/Git/JustTest.java
+++ c4uGitTest/src/main/java/com/my/Git/JustTest.java
@@ -3,6 +3,7 @@ package com.my.Git;
 public class JustTest {
 
 	public static void main(String[] args) {
-		System.out.println("헿");
+		System.out.println("�뿿");
+		System.out.println("Test");
 	}
 }
